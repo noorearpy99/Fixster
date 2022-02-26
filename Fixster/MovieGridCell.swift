b3b12b0a8e83102ad8c2f201bd527079c8e79e03
noorearpy99 <nuearpy1997@gmail.com>
2022-02-25 23:54:35 -0500

    updated app version 2

---
+++ Fixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Fixster
+//
+//  Created by Noor Earpy on 2/24/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
